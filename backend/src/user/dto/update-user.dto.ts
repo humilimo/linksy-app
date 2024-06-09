5d2a4ddd6a17b1f6349851117f6d5eff2c2e4a60
luanbezerra <luanbezerra@protonmail.com>
2024-06-09 16:46:36 -0300

    feat: update user dto

--- backend/src/user/dto/update-user.dto.ts
+++ backend/src/user/dto/update-user.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsEmail, IsNotEmpty, Length, MinLength, MaxLength, IsString, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsNotEmpty()
+    @IsString()
+    name:  string;
+    @IsNotEmpty()
+    username: string;
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;   
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+    bio: string;
+    picture: string;
+}

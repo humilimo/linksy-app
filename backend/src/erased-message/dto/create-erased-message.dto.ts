bb23715a257d909e5d8b8a922a9b5826ee4fc475
mms-11 <140762703+mms-11@users.noreply.github.com>
2024-06-10 17:08:26 -0300

    criando dto de mensagens apagadas

--- backend/src/erased-message/dto/create-erased-message.dto.ts
+++ backend/src/erased-message/dto/create-erased-message.dto.ts
@@ -1,1 +1,19 @@
-export class CreateErasedMessageDto {}
+export class CreateErasedMessageDto {
+    
+    
+
+        readonly id_mensagem: number;
+        readonly id_remetente: number;
+        readonly id_conversa: number;
+        readonly conteudo: String;
+        readonly momento_envio: String;
+    
+        constructor(msg: CreateErasedMessageDto) {
+          this.id_mensagem = msg.id_mensagem;
+          this.id_remetente = msg.id_remetente;
+          this.id_conversa = msg.id_conversa;
+          this.conteudo = msg.conteudo;
+          this.momento_envio = msg.momento_envio;
+      
+}
+}

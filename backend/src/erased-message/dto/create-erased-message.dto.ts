0731c3960ca7df5560c23d72812c7c1de02ef192
mms-11 <140762703+mms-11@users.noreply.github.com>
2024-06-11 11:22:53 -0300

    feat/erasing-messages

--- backend/src/erased-message/dto/create-erased-message.dto.ts
+++ backend/src/erased-message/dto/create-erased-message.dto.ts
@@ -1,1 +1,14 @@
-export class CreateErasedMessageDto {}
+export class CreateErasedMessageDto {
+   
+        readonly userId: number;
+        readonly chatId: number;
+        readonly messageId: number;
+      
+        constructor(msg: CreateErasedMessageDto) {
+          this.userId = msg.userId;
+          this.chatId = msg.chatId;
+          this.messageId = msg.messageId;
+         
+        }
+      
+}
--- backend/src/erased-message/erased-message.service.ts
+++ backend/src/erased-message/erased-message.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { CreateErasedMessageDto } from './dto/create-erased-message.dto';
 import { UpdateErasedMessageDto } from './dto/update-erased-message.dto';
+import { PrismaService } from '../prisma.service';
+import { ErasedMessage } from 'src/erased-message/entities/erased-message.entity';
 
 @Injectable()
 export class ErasedMessageService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createErasedMessageDto: CreateErasedMessageDto) {
     return 'This action adds a new erasedMessage';
   }
@@ -22,5 +26,30 @@ export class ErasedMessageService {
 
   remove(id: number) {
     return `This action removes a #${id} erasedMessage`;
+  }
+
+  async findMessageFromConversation(erased: ErasedMessage) {
+    return await this.prisma.message.findUnique({
+      where: {
+        userId_chatId_messageId: {
+          userId: erased.userId,
+          chatId: erased.chatId,
+          messageId: erased.messageId,
+        },
+      },
+    });
+  }
+
+  async removeErasedMessage(msg: ErasedMessage) {
+    const erased = await this.findMessageFromConversation(msg);
+    if (!erased) {
+      throw new Error('Message not found');
+    }
+
+    return this.prisma.message.delete({
+      where: {
+        id: erased.id,
+      },
+    });
   }
 }

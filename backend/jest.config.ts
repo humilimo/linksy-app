aa76c723449f4e3a7169abbfa804e0e38969d72b
humilimo <lfoc@cin.ufpe.br>
2024-06-16 15:15:37 -0300

    ✨ feat: jest testing sarting configuration

---
+++ backend/jest.config.ts
@@ -0,0 +1,22 @@
+import type {Config} from 'jest';
+
+const config: Config = {
+  verbose: true,
+  roots: ['<rootDir>/tests/steps'], // Define onde os testes estão localizados
+  testMatch: ['<rootDir>/tests/steps/*.steps.ts'], // Define o padrão de arquivos de teste
+  preset: 'ts-jest', // Usado para transpilar TypeScript para JavaScript antes de rodar os testes
+  testEnvironment: 'node', // Define o ambiente de teste (node ou jsdom para testes de front-end)
+  transform: {
+    '^.+\\.ts$': 'ts-jest', // Transforma arquivos TypeScript usando ts-jest
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'], // Define as extensões de arquivo que o Jest deve processar
+  collectCoverage: true, // Habilita a coleta de cobertura de código
+  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/**/*.d.ts'], // Define de onde coletar cobertura de código
+  coverageDirectory: 'coverage', // Define o diretório onde o relatório de cobertura será salvo
+  coverageReporters: ['text', 'lcov'], // Define os tipos de relatórios de cobertura a serem gerados
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1', // Mapeia caminhos para simplificar os imports
+  },
+};
+
+export default config;
---
+++ backend/tests/steps/example.steps.ts
@@ -0,0 +1,45 @@
+// Importações
+import { UserService } from '../../src/user/user.service';
+import { PrismaService } from '../../src/prisma.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { loadFeature, defineFeature } from 'jest-cucumber';
+
+const feature = loadFeature('features/example.feature');
+
+defineFeature(feature, (test) => {
+  let prismaService: PrismaService;
+  let userService: UserService;
+  let testingModule: TestingModule;
+
+  beforeAll(async () => {
+    testingModule = await Test.createTestingModule({
+      providers: [
+        UserService,
+        PrismaService,
+      ],
+    }).compile();
+
+    prismaService = testingModule.get<PrismaService>(PrismaService);
+    userService = testingModule.get<UserService>(UserService);
+  });
+
+  afterAll(async () => {
+    await testingModule.close();
+  });
+
+  test('Entering a correct password', async ({ given }) => {
+    given('I have previously created a password', async () => {
+      let mockUser = {
+          id: 0,
+          name: 'linksy',
+          username: 'linksy',
+          email: 'linksy',
+          password: 'linksy',
+          bio: "Tô!",
+          picture: null,
+      };
+      const user = await userService.findOne(mockUser.id);
+      expect(user).toEqual(mockUser);
+    });
+  });
+});

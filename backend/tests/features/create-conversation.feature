313aa96588f9ea6fd789f14adea433d83f38f25f
MateusNeeves <mateus1603@gmail.com>
2024-06-17 02:51:09 -0300

    ♻️ refactor: making steps reusable

--- backend/tests/features/create-conversation.feature
+++ backend/tests/features/create-conversation.feature
@@ -14,7 +14,7 @@ Feature: Create Conversation
   Scenario: Criar uma Conversa Simples
     Given existe um user com id "1" e username "man2"
     And existe um user com id "3" e username "crc"
-    When o corpo da requisição é um JSON com isGroup "false" e o array de inteiros ids
+    When o corpo da requisição é um JSON com isGroup "false", name "null", picture "null" e o array de inteiros ids
     And o array ids deve conter o inteiro "3"
     And uma requisição "POST" for enviada para "/user/1/conversation"
     Then o status da resposta deve ser "201"
@@ -26,7 +26,7 @@ Feature: Create Conversation
     And existe uma conversation com id "2" e isGroup "false"
     And existe uma userConversation com userId "1", conversationId "2", owner "false" e leftConversation "true"
     And existe uma userConversation com userId "2", conversationId "2" e owner "false"
-    When o corpo da requisição é um JSON com isGroup "false" e o array de inteiros ids
+    When o corpo da requisição é um JSON com isGroup "false", name "null", picture "null" e o array de inteiros ids
     And o array ids deve conter o inteiro "2"
     And uma requisição "POST" for enviada para "/user/1/conversation"
     Then o status da resposta deve ser "201"
--- backend/tests/steps/create-conversation.steps.ts
+++ backend/tests/steps/create-conversation.steps.ts
@@ -30,198 +30,141 @@ defineFeature(feature, (test) => {
 		
   });
 
-	// beforeEach(async () => {
-	// 	execSync('npx prisma migrate reset --force');
-	// });
-
   afterAll(async () => {
     await testingModule.close();
 		await app.close();
   });
 
+	// SHARED STEPS
 
-  test('Criar um Grupo', async ({given, when, then, and}) => {
-		given(/^existe um user com id "(.*)" e username "(.*)"$/, async function (id, username) {
-			let user = await prismaService.user.findUnique({
-				where: {
-					id: Number(id),
-					username: username
-				} 
+		const checkIfUserExistsStep = ({given}) => {
+			given(/^existe um user com id "(.*)" e username "(.*)"$/, async function (id, username) {
+				let user = await prismaService.user.findUnique({
+					where: {
+						id: Number(id),
+						username: username
+					} 
+				});
+				expect(user).not.toBeNull();
 			});
-			expect(user).not.toBeNull();
-		});
+		};
 
-		and(/^existe um user com id "(.*)" e username "(.*)"$/, async function (id, username) {
-			let user = await prismaService.user.findUnique({
-				where: {
-					id: Number(id),
-					username: username
-				} 
+		const checkJsonSteps = ({when}) => {
+			when(/^o corpo da requisição é um JSON com isGroup "(.*)", name "(.*)", picture "(.*)" e o array de inteiros ids$/, async function (isGroup, name, picture) {
+				request_json = {
+					"isGroup": (isGroup === 'true'),
+					"name": name == 'null' ? null : name,
+					"picture": picture == 'null' ? null : picture,
+					"ids": []
+				};
 			});
-			expect(user).not.toBeNull();
-		});
 
-
-		when(/^o corpo da requisição é um JSON com isGroup "(.*)", name "(.*)", picture "(.*)" e o array de inteiros ids$/, async function (isGroup, name, picture) {
-			request_json = {
-				"isGroup": (isGroup === 'true'),
-				"name": name == 'null' ? null : name,
-				"picture": picture == 'null' ? null : picture,
-				"ids": [],
-			};
-		});
-
-		and(/^o array ids deve conter o inteiro "(.*)"$/, async function (id) {
-			request_json.ids.push(Number(id));
-		});
-
-		and(/^uma requisição "POST" for enviada para "(.*)"$/, async function (route) {
-			response = await request(app.getHttpServer()).post(route).send(request_json);
-		});
-
-		then(/^o status da resposta deve ser "(.*)"$/, async function (status) {
-			expect(response.status).toBe(Number(status));
-		});
-
-		and(/^o JSON da resposta deve conter createMessage "(.*)"$/, async function (createMessage) {
-			expect(response.body.createMessage).toBe(createMessage);
-		});
-
-		and(/^o JSON da resposta deve conter o array de strings addMessages$/, async function () {
-			expect(response.body.addMessages).toBeDefined();
-		});
-
-		and(/^o array addMessages deve conter a string "(.*)"$/, async function (addMessage) {
-			expect(response.body.addMessages).toContain(addMessage);
-		});
-  });
-
-  test('Criar uma Conversa Simples', async ({given, when, then, and}) => {
-		given(/^existe um user com id "(.*)" e username "(.*)"$/, async function (id, username) {
-			let user = await prismaService.user.findUnique({
-				where: {
-					id: Number(id),
-					username: username
-				} 
+			when(/^o array ids deve conter o inteiro "(.*)"$/, async function (id) {
+				request_json.ids.push(Number(id));
 			});
-			expect(user).not.toBeNull();
-		});
+		};
 
-		and(/^existe um user com id "(.*)" e username "(.*)"$/, async function (id, username) {
-			let user = await prismaService.user.findUnique({
-				where: {
-					id: Number(id),
-					username: username
-				} 
+		const requestStep = ({when}) => {
+			when(/^uma requisição "POST" for enviada para "(.*)"$/, async function (route) {
+				response = await request(app.getHttpServer()).post(route).send(request_json);
 			});
-			expect(user).not.toBeNull();
-		});
+		};
 
-
-		when(/^o corpo da requisição é um JSON com isGroup "(.*)" e o array de inteiros ids$/, async function (isGroup) {
-			request_json = {
-				"isGroup": (isGroup === 'true'),
-				"ids": []
-			};
-		});
-
-		and(/^o array ids deve conter o inteiro "(.*)"$/, async function (id) {
-			request_json.ids.push(Number(id));
-		});
-
-		and(/^uma requisição "POST" for enviada para "(.*)"$/, async function (route) {
-			response = await request(app.getHttpServer()).post(route).send(request_json);
-		});
-
-		then(/^o status da resposta deve ser "(.*)"$/, async function (status) {
-			expect(response.status).toBe(Number(status));
-		});
-
-		and(/^o JSON da resposta deve conter beginMessage "(.*)"$/, async function (beginMessage) {
-			expect(response.body.beginMessage).toBe(beginMessage);
-		});
-  });
-
-  test('Retornar para uma Conversa Simples', async ({given, when, then, and}) => {
-		given(/^existe um user com id "(.*)" e username "(.*)"$/, async function (id, username) {
-			let user = await prismaService.user.findUnique({
-				where: {
-					id: Number(id),
-					username: username
-				} 
+		const checkStatusResponseStep = ({then}) => {
+			then(/^o status da resposta deve ser "(.*)"$/, async function (status) {
+				expect(response.status).toBe(Number(status));
 			});
-			expect(user).not.toBeNull();
-		});
+		};
 
-		and(/^existe um user com id "(.*)" e username "(.*)"$/, async function (id, username) {
-			let user = await prismaService.user.findUnique({
-				where: {
-					id: Number(id),
-					username: username
-				} 
+	// TESTS
+
+		test('Criar um Grupo', async ({given, when, then}) => {
+			checkIfUserExistsStep({given});
+			checkIfUserExistsStep({given});
+
+			checkJsonSteps({when});
+
+			requestStep({when});
+
+			checkStatusResponseStep({then});
+
+			then(/^o JSON da resposta deve conter createMessage "(.*)"$/, async function (createMessage) {
+				expect(response.body.createMessage).toBe(createMessage);
 			});
-			expect(user).not.toBeNull();
-		});
 
-		and(/^existe uma conversation com id "(.*)" e isGroup "(.*)"$/, async function (id, isGroup) {
-			let conversation = await prismaService.conversation.findUnique({
-				where: { 
-					id: Number(id),
-					isGroup: (isGroup === 'true')
-				} 
+			then(/^o JSON da resposta deve conter o array de strings addMessages$/, async function () {
+				expect(response.body.addMessages).toBeDefined();
 			});
-			expect(conversation).not.toBeNull();
-		});
 
-		and(/^existe uma userConversation com userId "(.*)", conversationId "(.*)", owner "(.*)" e leftConversation "(.*)"$/, async function (userId, conversationId, owner, leftConversation) {
-			let userConversation = await prismaService.userConversation.findUnique({
-				where: { 
-					userId_conversationId:{
-						userId: Number(userId),
-						conversationId: Number(conversationId),
-					},
-					owner: (owner === "true"),
-					leftConversation: (leftConversation === 'true')
-				} 
+			then(/^o array addMessages deve conter a string "(.*)"$/, async function (addMessage) {
+				expect(response.body.addMessages).toContain(addMessage);
 			});
-			expect(userConversation).not.toBeNull();
 		});
 
-		and(/^existe uma userConversation com userId "(.*)", conversationId "(.*)" e owner "(.*)"$/, async function (userId, conversationId, owner) {
-			let userConversation = await prismaService.userConversation.findUnique({
-				where: { 
-					userId_conversationId:{
-						userId: Number(userId),
-						conversationId: Number(conversationId),
-					},
-					owner: (owner === "true")
-				} 
+		test('Criar uma Conversa Simples', async ({given, when, then}) => {
+			checkIfUserExistsStep({given});
+			checkIfUserExistsStep({given});
+
+			checkJsonSteps({when});
+
+			requestStep({when});
+			
+			checkStatusResponseStep({then});
+
+			then(/^o JSON da resposta deve conter beginMessage "(.*)"$/, async function (beginMessage) {
+				expect(response.body.beginMessage).toBe(beginMessage);
 			});
-			expect(userConversation).not.toBeNull();
 		});
 
+		test('Retornar para uma Conversa Simples', async ({given, when, then}) => {
+			checkIfUserExistsStep({given});
+			checkIfUserExistsStep({given});
 
-		when(/^o corpo da requisição é um JSON com isGroup "(.*)" e o array de inteiros ids$/, async function (isGroup) {
-			request_json = {
-				"isGroup": (isGroup === 'true'),
-				"ids": []
-			};
-		});
+			given(/^existe uma conversation com id "(.*)" e isGroup "(.*)"$/, async function (id, isGroup) {
+				let conversation = await prismaService.conversation.findUnique({
+					where: { 
+						id: Number(id),
+						isGroup: (isGroup === 'true')
+					} 
+				});
+				expect(conversation).not.toBeNull();
+			});
 
-		and(/^o array ids deve conter o inteiro "(.*)"$/, async function (id) {
-			request_json.ids.push(Number(id));
-		});
+			given(/^existe uma userConversation com userId "(.*)", conversationId "(.*)", owner "(.*)" e leftConversation "(.*)"$/, async function (userId, conversationId, owner, leftConversation) {
+				let userConversation = await prismaService.userConversation.findUnique({
+					where: { 
+						userId_conversationId:{
+							userId: Number(userId),
+							conversationId: Number(conversationId),
+						},
+						owner: (owner === "true"),
+						leftConversation: (leftConversation === 'true')
+					} 
+				});
+				expect(userConversation).not.toBeNull();
+			});
 
-		and(/^uma requisição "POST" for enviada para "(.*)"$/, async function (route) {
-			response = await request(app.getHttpServer()).post(route).send(request_json);
-		});
+			given(/^existe uma userConversation com userId "(.*)", conversationId "(.*)" e owner "(.*)"$/, async function (userId, conversationId, owner) {
+				let userConversation = await prismaService.userConversation.findUnique({
+					where: { 
+						userId_conversationId:{
+							userId: Number(userId),
+							conversationId: Number(conversationId),
+						},
+						owner: (owner === "true"),
+					} 
+				});
+				expect(userConversation).not.toBeNull();
+			});
 
-		then(/^o status da resposta deve ser "(.*)"$/, async function (status) {
-			expect(response.status).toBe(Number(status));
-		});
+			checkJsonSteps({when});
 
-		and(/^o JSON da resposta deve conter returnMessage "(.*)"$/, async function (returnMessage) {
-			expect(response.body.returnMessage).toBe(returnMessage);
+			requestStep({when});
+			
+			checkStatusResponseStep({then});
+
+			then(/^o JSON da resposta deve conter returnMessage "(.*)"$/, async function (returnMessage) {
+				expect(response.body.returnMessage).toBe(returnMessage);
+			});
 		});
-  });
 });

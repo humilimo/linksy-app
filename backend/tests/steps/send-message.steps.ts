0dc90cf18701b7a5b92e4895233c8718123bfc16
humilimo <lfoc@cin.ufpe.br>
2024-06-17 21:23:02 -0300

    ✨feat: send-message step

--- backend/tests/steps/send-message.steps.ts
+++ backend/tests/steps/send-message.steps.ts
@@ -1,41 +1,56 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest'
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { MessageController } from '../../src/message/message.controller';
 import { ConversationController } from '../../src/conversation/conversation.controller';
 import { ConversationService } from '../../src/conversation/conversation.service';
 import { MessageService } from '../../src/message/message.service';
 import { UserConversationService } from '../../src/user-conversation/user-conversation.service';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../../src/prisma.service';
+import {MessageModule} from '../../src/message/message.module';
+import { UserService } from '../../src/user/user.service';
 
-const feature = loadFeature('tests/features/get-conversation-messages.feature');
+const feature = loadFeature('tests/features/send-message.feature');
 
 defineFeature(feature, (test) => {
   let testingModule: TestingModule;
   let prismaService: PrismaService;
-    let app: INestApplication;
-    let response: any;
+  let app: INestApplication;
+  let response: any;
+  let request_json: any;
 
   beforeAll(async () => {
     testingModule = await Test.createTestingModule({
-            controllers: [ConversationController],
-            providers: [PrismaService, ConversationService, UserConversationService, MessageService]
+        controllers: [MessageController],
+        providers: [PrismaService, MessageService, ConversationService, UserConversationService]
     }).compile();
         
     prismaService = testingModule.get<PrismaService>(PrismaService);
-        app = testingModule.createNestApplication();
-        await app.init();
-        
+    app = testingModule.createNestApplication();
+    await app.init();
   });
 
   afterAll(async () => {
     await testingModule.close();
-        await app.close();
+    await app.close();
   });
 
     // SHARED STEPS
         
     const checkIfUserExists = ({given}) => {
+        given(/^existe um user com id "(.*)", username "(.*)"$/, async function (id, username) {
+            let user = await prismaService.user.findUnique({
+                where: {
+                    id: Number(id),
+                    username: username
+                } 
+            });
+            expect(user).not.toBeNull();
+        });
+    };
+
+    const checkIfSecondUserExists = ({given}) => {
         given(/^existe um user com id "(.*)", username "(.*)"$/, async function (id, username) {
             let user = await prismaService.user.findUnique({
                 where: {
@@ -75,27 +90,59 @@ defineFeature(feature, (test) => {
         });
     };
 
+    const checkIfSecondUserConversationExists = ({given}) => {
+        given(/^existe uma userConversation com userId "(.*)", conversationId "(.*)"$/, async function (userId, conversationId) {
+            let userConversation = await prismaService.userConversation.findUnique({
+                where: {
+                    userId_conversationId:{
+                        userId: Number(userId),
+                        conversationId: Number(conversationId)
+                    }
+                } 
+            });
+            expect(userConversation).not.toBeNull();
+        });
+    };
+
+    const requestStep = ({when}) => {
+        when(/^uma requisição "POST" for enviada para "(.*)"$/, async function (route) {
+            response = await request(app.getHttpServer()).post(route).send(request_json);
+        });
+    };
+
+    const checkJsonSteps = ({when}) => {
+        when(/^o corpo da requisição é um JSON com content "(.*)" e createdAt "(.*)"$/, async function (content, createdAt) {
+            request_json = {
+                "content": content,
+                "createdAt": createdAt
+            };
+        });
+    };
+
+    const checkStatusResponseStep = ({then}) => {
+        then(/^o status da resposta deve ser "(.*)"$/, async function (status) {
+            expect(response.status).toBe(Number(status));
+        });
+    };
+
     // SCENARIO: Enviar mensagens em conversas
 
     test('Enviar mensagens em conversas', ({ given, when, then }) => {
         checkIfUserExists({ given });
+        checkIfSecondUserExists({ given });
         checkIfConversationExists({ given });
         checkIfUserConversationExists({ given });
+        checkIfSecondUserConversationExists({ given });
+        
+        checkJsonSteps({ when });
 
-        when(/^uma requisição "POST" for enviada para "(.*)"$/, async function (endpoint) {
-            response = await request(app.getHttpServer()).post(endpoint);
-        });
+        requestStep({ when });
 
-        when(/^o corpo da requisição é um JSON com content "(.*)", createdAt "(.*)"$/, async function (content, createdAt) {
-            response.send({ content: content, createdAt: createdAt });
-        });
+        checkStatusResponseStep({ then });
 
-        then(/^o status da resposta deve ser "(.*)"$/, function (statusCode) {
-            expect(response.status).toBe(Number(statusCode));
-        });
-
-        then(/^o JSON da resposta deve conter content "(.*)", senderId "(.*)", conversationId "(.*)", createdAt "(.*)"$/, function (content, senderId, conversationId, createdAt) {
-            expect(response.body).toEqual({
+        then(/^o JSON da resposta deve conter content "(.*)", senderId "(.*)", conversationId "(.*)" e createdAt "(.*)"$/, function (content, senderId, conversationId, createdAt) {
+            const {id, ...restBody} = response.body
+            expect(restBody).toEqual({
                 content: content,
                 senderId: Number(senderId),
                 conversationId: Number(conversationId),

48dd22f7b2152f35399e749417a85aef5517f397
humilimo <lfoc@cin.ufpe.br>
2024-07-01 08:20:22 -0300

    ✨feat: frontend boilerplate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import CreateTest from "./app/home/pages/CreateTest";
-import ListTests from "./app/home/pages/ListTests";
+import { Router } from "./router/index";
 
-const router = createBrowserRouter([
-  {
-    path: "*",
-    Component: CreateTest,
-  },
-  {
-    path: "/create-test",
-    Component: CreateTest,
-  },
-  {
-    path: "/tests",
-    Component: ListTests,
-  },
-]);
-
-export default function App() {
-  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+export function App() {
+  return (
+    <>
+      <Router/>
+    </>
+  );
 }
---
+++ frontend/src/router/index.tsx
@@ -0,0 +1,10 @@
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+export function Router() {
+    return (
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>
+    );
+  }

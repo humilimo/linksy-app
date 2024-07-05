73636f052033bbe66640cfbfd226f29d14a4bcc7
MateusNeeves <mateus1603@gmail.com>
2024-07-04 23:45:18 -0300

    feat: front-end for simple conversation details and groupName verification in group deletion

--- frontend/src/app/home/pages/ConversationProfile/index.tsx
+++ frontend/src/app/home/pages/ConversationProfile/index.tsx
@@ -19,6 +19,8 @@ const ConversationProfileMenu = () => {
   const [user, setUser] = useState<UserProps | null>(null);
 
   const [showDeleteGroupModal, setShowDeleteGroupModal] = React.useState(false);
+  const [groupName, setGroupName] = React.useState<string | null>(null);
+  const [wrongGroupName, setWrongGroupName] = React.useState<boolean | false>(false);
 
   const fetchConversationProfileData = async () => {
     try {
@@ -42,14 +44,18 @@ const ConversationProfileMenu = () => {
     fetchConversationProfileData();
   }, [loggedId, conversationId]);
 
-  const handleGroupDeletion = async () =>{ 
+  const submitGroupDeletion = async (e) =>{
+    e.preventDefault();
     try {
       const response = await axios.delete(
-        `http://127.0.0.1:3002/user/${loggedId}/conversation/${conversationId}/delete_all`
+        `http://127.0.0.1:3002/user/${loggedId}/conversation/${conversationId}/delete_all`, {data: {groupName: groupName}}
       );
       if (response.data.destroyMessage){
         let path = `/user/${loggedId}/conversation`; 
         navigate(path, {state:{destroyMessage:response.data.destroyMessage}});
+      }
+      else if (response.data.wrongNameMessage){
+        setWrongGroupName(true);
       }
     } catch (error) {
       console.log(error);
@@ -107,20 +113,25 @@ const ConversationProfileMenu = () => {
 
             <div className='flex-1 bg-gray-100 mx-4 px-6 py-4 rounded-lg shadow-lg overflow-y-auto'>
               <ul className="list-disc list-inside">
-                {participants.map((participant) => (
-                  <li key={participant.id} className="text-gray-700 mb-2 flex items-center">
-                    <div className='relative pe-4'>
-                      {participant.picture ? (
-                        <img src={participant.picture} alt={participant.name} className="w-10 h-10 rounded-full"/>
-                      ) : (
-                        <div className='relative'>
-                          <BsCircle className="w-12 h-12"/>
-                          <BsPeopleFill className="w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
-                        </div>
-                      )}
+                {participants.map((participant, index) => (
+                  <li key={participant.id} className="text-gray-700 mb-2 flex items-center justify-between">
+                    <div className='flex items-center'>
+                      <div className='relative pe-4'>
+                        {participant.picture ? (
+                          <img src={participant.picture} alt={participant.name} className="w-10 h-10 rounded-full"/>
+                        ) : (
+                          <div className='relative'>
+                            <BsCircle className="w-12 h-12"/>
+                            <BsPeopleFill className="w-8 h-8 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
+                          </div>
+                        )}
+                      </div>
+                      <p className='pe-4'>{participant.name}</p>
+                      <p className='pe-4'>({participant.username})</p>
                     </div>
-                    <p className='pe-4'>{participant.name}</p>
-                    <p className='pe-4'>({participant.username})</p>
+                    {index == 0 ? (
+                      <p className='justify-end text-gray-500'>Dono</p>
+                    ) : null}
                   </li>
                 ))}
               </ul>
@@ -128,19 +139,24 @@ const ConversationProfileMenu = () => {
             
             <div className='pb-[50px] pt-6 flex justify-end'>
               {owner ? (
-                <button className="text-center text-white py-2 px-5 rounded-2xl bg-red-600 hover:bg-red-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={() => setShowDeleteGroupModal(true)}>
+                <button className="text-center text-white py-2 px-5 me-8 rounded-2xl bg-red-600 hover:bg-red-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={() => setShowDeleteGroupModal(true)}>
                   Deletar Grupo
                 </button>
-              ) : null}
+              ) : (
+                <button className="text-center text-white py-2 px-5 me-8 rounded-2xl bg-red-600 hover:bg-red-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
+                Sair do Grupo
+              </button>
+              )}
 
               {showDeleteGroupModal ? (
               <>
                 <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                  <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                  <form onSubmit={submitGroupDeletion} className="relative w-auto my-6 mx-auto max-w-3xl">
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                      {/* HEADER */}
                       <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
                         <h3 className="text-3xl font-semibold">
-                          Confirmar deleção do grupo
+                          Deseja deletar o grupo?
                         </h3>
                         <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={() => setShowDeleteGroupModal(false)}>
                           <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
@@ -148,21 +164,34 @@ const ConversationProfileMenu = () => {
                           </span>
                         </button>
                       </div>
+                      {/* BODY */}
                       <div className="relative p-6 flex-auto">
-                        <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                          Certeza que deseja deletar o grupo?
+                        <p className="mb-1 text-blueGray-500 text-lg leading-relaxed text-center">
+                          Para confirmar a deleção do grupo, escreva abaixo <br /> o nome do grupo e clique em 'Deletar' 
                         </p>
+                        { wrongGroupName ? (
+                          <p className='text-red-500'>
+                            Nome do Grupo Incorreto
+                          </p>
+                        ) : null}
+                        
+                        <input name='groupName' id='groupName' onChange={(e)=>{setGroupName(e.target.value)}} type="text" className='rounded shadow border border-gray-200 px-1 py-2 mt-3 w-full' />
                       </div>
+                      {/* FOOTER */}
                       <div className="flex items-center justify-between p-6 border-t border-solid border-blueGray-200 rounded-b">
-                        <button className="bg-gray-600 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:bg-gray-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={() => setShowDeleteGroupModal(false)}>
+                        <button className="bg-gray-600 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:bg-gray-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" 
+                        onClick={() => {
+                          setShowDeleteGroupModal(false); 
+                          setWrongGroupName(false);
+                        }}>
                           Cancelar
                         </button>
-                        <button className="bg-red-600 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:bg-red-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={handleGroupDeletion}>
+                        <button className="bg-red-600 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:bg-red-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="submit">
                           Deletar
                         </button>
                       </div>
                     </div>
-                  </div>
+                  </form>
                 </div>
                 <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
               </>
@@ -170,23 +199,33 @@ const ConversationProfileMenu = () => {
             </div>
           </div>
         ) : user ? (
-          <div className="p-6 pt-8 bg-gray-100 min-h-screen">
-            <div className='bg-white p-6 rounded-lg shadow-lg'>
-              <div className="flex items-center justify-center pb-8">
-                {user.picture ? (
-                  <img src={user.picture} className="w-16 h-16 rounded-full"/>
-                ) : (
-                  <div className="relative">
-                    <BsCircle className="w-28 h-28"/>
-                    <BsPeopleFill className="w-20 h-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
-                  </div>
-                )}
-                <p className="ps-8 text-4xl font-bold">{user.name} ({user.username})</p>
-              </div>
+          <div className='flex flex-col h-screen'>
+            <div className='p-6 flex flex-col items-center'>
+              {user.picture ? (
+                <img src={user.picture} className="w-16 h-16 rounded-full"/>
+              ) : (
+                <div className="relative">
+                  <BsCircle className="w-40 h-40"/>
+                  <BsPeopleFill className="w-28 h-28 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
+                </div>
+              )}
+              <p className="text-2xl text-center font-bold pt-6">{user.name}</p>
+              <p className="text-md text-center font-bold pt-1">({user.username})</p>
+              <p className="text-md text-center pt-1">{user.email}</p>
+            </div>
 
-              <p className="text-gray-700">Email: {user.email}</p>
-              <p className="text-gray-700">Bio: {user.bio}</p>
-              <p className="text-gray-700">Bio: {user.bio}</p>
+            <div className='ps-8 pb-2'>
+              <h2 className="text-2xl font-semibold">Bio:</h2>
+            </div>
+
+            <div className='flex-1 bg-gray-100 mx-4 px-6 py-4 rounded-lg shadow-lg overflow-y-auto'>
+              <p className="text-gray-700">{user.bio}</p>
+            </div>
+
+            <div className='pb-[50px] pt-6 flex justify-end'>
+              <button className="text-center text-white py-2 px-5 me-8 rounded-2xl bg-red-600 hover:bg-red-500 hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" >
+                Excluir Conversa
+              </button>
             </div>
           </div>
         ) : (

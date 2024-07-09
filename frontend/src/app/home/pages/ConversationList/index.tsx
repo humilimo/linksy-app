bfd2ff421b9958c6a0e0c6578dc611521b585e04
MateusNeeves <mateus1603@gmail.com>
2024-07-09 14:37:06 -0300

    feat: create simple conversation modularized

--- frontend/src/app/home/pages/ConversationList/index.tsx
+++ frontend/src/app/home/pages/ConversationList/index.tsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
+
+import CreateSimpleConversationModal from "../../../../components/ConversationList/CreateSimpleConversationModal"
 
 
 function ConversationList() {
   const location = useLocation();
+
+  const { loggedId } = useParams<{ loggedId: string }>();
+
+  const [showFriendListSelectionModal, setShowFriendListSelectionModal] = useState(false);
 
   return (
     <div className="p-6 pt-8 bg-gray-100 min-h-screen">
@@ -17,11 +21,20 @@ function ConversationList() {
             <p> {location.state.destroyMessage}</p>
           ) : null}
         </div>
+
+        {/* NEW SIMPLE CONVERSATION BUTTON */}
+        <div className='pb-[50px] pt-6 flex justify-end'>
+          <button className="text-center text-white py-2 px-5 mr-8 rounded-2xl bg-green-600 hover:bg-green-500 hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150" type="button" onClick={() => setShowFriendListSelectionModal(true)}>
+            Nova Conversa
+          </button>
+
+          {showFriendListSelectionModal ? (
+            <CreateSimpleConversationModal loggedId={loggedId} setShowFriendListSelectionModal={setShowFriendListSelectionModal}/>
+          ) : null}
         </div>
+      </div>
     </div>
   );
-
-  
 }
 
 export default ConversationList;

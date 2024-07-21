60928a302031a7525a3809298d5d8a08145681c4
humilimo <lfoc@cin.ufpe.br>
2024-07-21 16:36:52 -0300

    💄style: flex-grow on some components based on position

--- frontend/src/app/components/MessageBox/MessageBox.tsx
+++ frontend/src/app/components/MessageBox/MessageBox.tsx
@@ -1,11 +1,11 @@
-import { format } from 'date-fns';
+import { format, isToday } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { CgProfile } from "react-icons/cg";
 
 const MessageBox: React.FC<MessageBoxModel> = ({ message, senderInfo, isOwnMessage }) => (
     <div className={`flex ${isOwnMessage ? 'justify-end' : 'justify-start'} mb-4`}>
       <div className='flex flex-col gap-1 min-w-40'>
-        <div className={`flex gap-2 ${isOwnMessage ? 'flex-row-reverse':'flex-row'}`}>
+        <div className={`flex gap-2 items-end ${isOwnMessage ? 'flex-row-reverse':'flex-row'}`}>
           {senderInfo.picture
             ? <img
                 src={senderInfo.picture}
@@ -14,17 +14,17 @@ const MessageBox: React.FC<MessageBoxModel> = ({ message, senderInfo, isOwnMessa
               />
             : <CgProfile className="w-8 h-8 text-gray-500" />
           }
-          <p className="text-xs text-black mt-2">
-            {new Date(message.createdAt).toLocaleTimeString('pt-BR', {
-              hour: '2-digit',
-              minute: '2-digit'
-            })}
+          <p className={`font-bold ${isOwnMessage ? null : 'flex-grow'}`}>{senderInfo.name}</p>
+          <p className={` text-black text-[12px] ${isOwnMessage ? 'flex-grow':null}`}>
+            {
+              new Date(message.createdAt).toLocaleTimeString('pt-BR', {
+                hour: '2-digit',
+                minute: '2-digit'
+              })
+            }
           </p>
         </div>
         <div className={`max-w-xs p-3 rounded-lg ${isOwnMessage ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black'} relative`}>
-          {!isOwnMessage && (
-            <p className="text-sm font-bold mb-1">{senderInfo.name}</p>
-          )}
           <p className="text-sm break-words">{message.content}</p>
         </div>
       </div>

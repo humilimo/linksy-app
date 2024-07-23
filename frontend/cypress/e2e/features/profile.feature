4efc7dc936cfd3b5d69bec6c7305fa9ead74abfe
luanbezerra <luanbezerra@protonmail.com>
2024-07-23 20:39:38 -0300

    feat: a lot of forgotten stuff

---
+++ frontend/cypress/e2e/features/profile.feature
@@ -0,0 +1,29 @@
+Feature: Profile
+
+    # GUI Scenarios
+
+    Scenario: Edição de nome com sucesso
+        Given o usuário está logado com o usuário de id "7", username "lbc2" e senha "luan12345"
+        When uma requisição PATCH com um JSON com name "Luisinho" de corpo
+        And esta requisição for enviada para "user/2/profile"
+        Then o status da resposta deve ser "200"
+        And o JSON da resposta deve conter id "2", name "Luisinho", username "lfoc", email "lfoc@cin.ufpe.br", password "luis12345", bio "null" e picture "null"
+
+    Scenario: Edição de email sem sucesso
+        Given existe um user com id "2"
+        When uma requisição PATCH com um JSON com email "crc@cin.ufpe.br" de corpo
+        And esta requisição for enviada para "user/2/profile"
+        Then o status da resposta deve ser "400"
+        And o JSON deve conter message "Este email já está em uso." e error "Bad Request"
+
+    Scenario: Visualização do próprio perfil
+        Given existe um user com id "3"
+        When uma requisição GET foi enviada para "user/3/profile"
+        Then o status da resposta deve ser "200"
+        And o JSON da resposta deve conter id "3", name "Caio", username "crc", bio "null" e picture "null"
+
+    Scenario: Visualização do perfil de um amigo
+        Given existe um user com id "2" que quer ver o perfil do seu amigo de id "3"
+        When uma requisição GET foi enviada para "user/2/profile/3"
+        Then o status da resposta deve ser "200"
+        And o JSON da resposta deve conter id "3", name "Caio", username "crc", bio "null" e picture "null"
--- frontend/src/app/components/AccountCreation/Form2.tsx
+++ frontend/src/app/components/AccountCreation/Form2.tsx
@@ -15,49 +15,58 @@ const Form2: React.FC<FormStep2Props> = ({ formData, handleChange, handlePrev, h
     return (
         <div> 
             <div className='bg-white px-10 py-10 rounded-3xl border-2 border-gray-200'>
-                <h1 className='text-5xl font-semibold'>Linksy | Sign Up</h1>
-                <p className='font-medium text-lg text-gray-500 mt-4'>Create a password of at least 8 characters</p>
+                <h1 className='text-5xl font-semibold'>Linksy</h1>
+                <h3 className='text-2xl font-semibold mt-3'>Cadastro</h3>
+                <p className='font-medium text-lg text-gray-500 mt-4'>Crie uma senha de até 8 caracteres</p>
                 <div className='mt-8'>
                     <div className='pb-5'>
-                    <label className='text-lg font-medium'>Password</label>
+                    <label className='text-lg font-medium'>Senha</label>
                     <input
                         name='password'
                         id='password'
                         value={formData.password}
                         onChange={handleChange}
                         className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-                        placeholder='Your password'
-                        type="password"/>
+                        placeholder='Sua senha'
+                        type="password"
+                        data-cy={"sign-up-password"}/>
                     </div>
                     <div className='pb-5'>
-                    <label className='text-lg font-medium'>Confirm your password</label>
+                    <label className='text-lg font-medium'>Confirme sua senha</label>
                     <input
                         name='passConfirmation'
                         id='passConfirmation'
                         value={formData.passConfirmation}
                         onChange={handleChange}
                         className='w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent'
-                        placeholder='Your password'
-                        type="password"/>
+                        placeholder='Confirmação da senha'
+                        type="password"
+                        data-cy={"sign-up-passConfirmation"}/>
                     </div>
                     {error2 && (
-                        <p className='text-red-500 text-sm mt-2'>{error2}</p>
+                        <p
+                            className='text-red-500 text-sm mt-2'
+                            data-cy={`sign-up-error-${error2 == "Sua senha tem menos de 8 caracteres" ? ('2'): ('3')}`} >
+                            
+                            {error2}
+                        </p>
                     )}
                     <div className='mt-8 flex space-x-4'>
                         <button 
                             className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl border-2 border-blue-500 text-blue-500 text-lg font-bold w-1/2'
                             onClick={handlePrev}> 
-                            Back
+                            Voltar
                         </button>
                         <button 
                             className='active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-blue-500 text-white text-lg font-bold w-1/2'
-                            onClick={handleSubmit}> 
-                            Sign Up
+                            onClick={handleSubmit}
+                            data-cy={"sign-up-button"}> 
+                            Cadastrar
                         </button>
                     </div>
                     <div className='mt-8 flex justify-center items-center'>
-                        <p className='font-medium text-base'>Already have an account?</p>
-                        <button className='hover:underline text-blue-500 text-base font-medium ml-2'>Sign in here!</button>
+                        <p className='font-medium text-base'>Já tem uma conta?</p>
+                        <button className='hover:underline text-blue-500 text-base font-medium ml-2'>Entre aqui!</button>
                     </div>
                 </div>
             </div>

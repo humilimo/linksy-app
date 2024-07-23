4f559ba93891b49fe0f0684af41832074b26f507
MateusNeeves <mateus1603@gmail.com>
2024-07-23 16:27:00 -0300

    fix: changing z position from conversation profile

--- frontend/src/app/components/ConversationProfile/AddParticipantToGroupModal.tsx
+++ frontend/src/app/components/ConversationProfile/AddParticipantToGroupModal.tsx
@@ -30,9 +30,9 @@ function AddParticipantToGroupModal(props) {
 
   return (
     <>
-    <div className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none h-full">
-      <div className="relative w-auto my-6 mx-auto max-w-3xl h-[calc(100%-1rem)]">
-        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none max-h-[100%]">
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="relative w-auto my-6 mx-auto max-w-3xl">
+        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
           {/* HEADER */}
           <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
             <h3 className="text-3xl font-semibold">
